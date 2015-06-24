75d58ffe96f9c67b4eaaba34d2a366e5242837f8
Vince Elizaga <vncelizaga@gmail.com>
2015-06-24 21:31:38 +0800

    update services

--- www/js/services.js
+++ www/js/services.js
@@ -3,12 +3,12 @@ angular.module('starter.services', [])
 .factory('Devices', function($http) {
   // Might use a resource here that returns a JSON array
 
-  var ipServer = 'http://esp8266-relays.herokuapp.com/';
+  var ipServer = 'http://esp8266-relays.herokuapp.com';
 
   return {
     deviceCommand: function(data) {
       console.log(data);
-      return $http.post('http://' + ipServer + '/device/control', data);
+      return $http.post(ipServer + '/device/control', data);
     }
   };
 });
